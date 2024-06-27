7441886036b572a764639a180e4973351eaa262c
hasecilu <hasecilu@tuta.io>
2024-06-27 13:04:32 -0600

    fix: use QT_TRANSLATE_NOOP() on property group titles to get proper encoding
    - Previously the usage of `translate()` provoked bad encoding.
    - Also properties were marked for translation. There is a bug that makes
      only properties with a single word (denoted by PascalCase) are
      translated. Two or more words are preserved on English.
    - Remove `TranslateUtils.py` file, use `FreeCAD.Qt.translate`
    - Update translation files to attend changes. Frech & Japanese
      micro-translations were added for testing purposes
    
    BREAKING CHANGE:
    - Properties and parameters were renamed to use PascalCase instead of
      camelCase, this is the FreeCAD way. Old files will fail to load.

---
+++ freecad/freegrid/resources/translations/FreeGrid_ja.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja" sourcelanguage="en">
+<context>
+    <name>App::Property</name>
+    <message>
+        <location filename="../../FreeGridCmd.py" line="30"/>
+        <source>Width</source>
+        <translation>幅</translation>
+    </message>
+    <message>
+        <location filename="../../FreeGridCmd.py" line="85"/>
+        <location filename="../../FreeGridCmd.py" line="37"/>
+        <location filename="../../FreeGridCmd.py" line="31"/>
+        <source>Size</source>
+        <translation>大きさ</translation>
+    </message>
+    <message>
+        <location filename="../../FreeGridCmd.py" line="32"/>
+        <source>Number of 50[mm] units in X direction</source>
+        <translation>X方向50[mm]単位数</translation>
+    </message>
+    <message>
+        <location filename="../../FreeGridCmd.py" line="36"/>
+        <source>Depth</source>
+        <translation>深さ</translation>
+    </message>
+    <message>
+        <location filename="../../FreeGridCmd.py" line="84"/>
+        <source>Height</source>
+        <translation>高さ</translation>
+    </message>
+</context>
+</TS>
