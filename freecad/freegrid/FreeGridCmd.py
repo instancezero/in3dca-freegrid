e0f57479080cef4d0c382b65e29a38202bab1e33
hasecilu <hasecilu@tuta.io>
2024-11-10 11:57:17 -0600

    fix(box/grid): set limits on integer and length properties
    - Use `App::PropertyIntegerConstraint` instead of `App::PropertyInteger`
    in order to get a smaller set of valid options for the parts.
    
    Minimum, maximum, step and default values are set.
    
    Previously you could enter negative numbers which were fixed at the time
    of generating the shape avoiding problems but the properties were not
    fixed. Now users are not able to enter bad data which leads to a
    simplification of the sanitization code.
    
    Proposed upper limit at 50. Seems sane value.
    
    - Created method to check length properties are inside limits.

--- freecad/freegrid/FreeGridCmd.py
+++ freecad/freegrid/FreeGridCmd.py
@@ -21,19 +21,33 @@ class StorageObject:
 
     def __init__(self, obj):
         """Initialize common properties."""
+
         self.storageType = ""
+        self.min_len_constraints = {}
+        self.max_len_constraints = {}
+
         obj.addProperty(
-            "App::PropertyInteger",
+            "App::PropertyIntegerConstraint",
             QT_TRANSLATE_NOOP("App::Property", "Width"),  # property
             QT_TRANSLATE_NOOP("App::Property", "Size"),  # group
             QT_TRANSLATE_NOOP("App::Property", "Number of 50[mm] units in X direction"),  # tooltip
-        ).Width = 1
+        ).Width = (
+            1,
+            1,
+            50,
+            1,
+        )  # (Default, Minimum, Maximum, Step size)
         obj.addProperty(
-            "App::PropertyInteger",
+            "App::PropertyIntegerConstraint",
             QT_TRANSLATE_NOOP("App::Property", "Depth"),
             QT_TRANSLATE_NOOP("App::Property", "Size"),
             QT_TRANSLATE_NOOP("App::Property", "Number of 50[mm] units in Y direction"),
-        ).Depth = 1
+        ).Depth = (
+            1,
+            1,
+            50,
+            1,
+        )  # (Default, Minimum, Maximum, Step size)
         obj.addProperty(
             "App::PropertyLength",
             QT_TRANSLATE_NOOP("App::Property", "MagnetDiameter"),
@@ -49,6 +63,25 @@ class StorageObject:
 
         obj.addExtension("Part::AttachExtensionPython")
         obj.AttacherEngine = "Engine Plane"
+
+    def check_limits(self, obj, prop):
+        """Check if the property being modified is in the dictionaries and apply the constraint"""
+        # Old-file compatibility
+        if not hasattr(self, "min_len_constraints"):
+            self.min_len_constraints = {}
+        if not hasattr(self, "max_len_constraints"):
+            self.max_len_constraints = {}
+
+        if (
+            prop in self.min_len_constraints
+            and getattr(obj, prop).getValueAs("mm").Value < self.min_len_constraints[prop]
+        ):
+            setattr(obj, prop, str(self.min_len_constraints[prop]) + "mm")
+        if (
+            prop in self.max_len_constraints
+            and getattr(obj, prop).getValueAs("mm").Value > self.max_len_constraints[prop]
+        ):
+            setattr(obj, prop, str(self.max_len_constraints[prop]) + "mm")
 
     def descriptionStr(self, obj):
         """Return the designation of the storage object."""
@@ -72,12 +105,25 @@ class StorageBoxObject(StorageObject):
 
     def __init__(self, obj):
         """Initialize storage box object, add properties."""
+
         super().__init__(obj)
+
         self.storageType = "StorageBox"
         self.magnetOptions = ["allIntersections", "cornersOnly", "noMagnets"]
-        obj.Depth = paramFreeGrid.GetInt("BoxDepth", 1)
-        obj.Width = paramFreeGrid.GetInt("BoxWidth", 1)
+
+        # Define value constraints for length properties
+        self.min_len_constraints = {
+            "Height": 2.6,  # No point having less than 2.6[mm] because geometry remains the same
+            "MagnetDiameter": 1,
+            "MagnetHeight": 0.2,
+            "BoxGripDepth": 1,
+        }
+        self.max_len_constraints = {"MagnetDiameter": 6.9, "MagnetHeight": 3.4}
+
+        obj.Depth = (paramFreeGrid.GetInt("BoxDepth", 1), 1, 50, 1)
+        obj.Width = (paramFreeGrid.GetInt("BoxWidth", 1), 1, 50, 1)
         obj.Proxy = self
+
         # TODO: check if it works when default system is imperial
         obj.addProperty(
             "App::PropertyLength",
@@ -89,17 +135,17 @@ class StorageBoxObject(StorageObject):
             ),
         ).Height = paramFreeGrid.GetString("BoxHeight", "50mm")
         obj.addProperty(
-            "App::PropertyInteger",
+            "App::PropertyIntegerConstraint",
             QT_TRANSLATE_NOOP("App::Property", "DivisionsX"),
             QT_TRANSLATE_NOOP("App::Property", "Internal divisions"),
             QT_TRANSLATE_NOOP("App::Property", "Number of divisions along the X axis"),
-        ).DivisionsX = paramFreeGrid.GetInt("DivisionsX", 1)
+        ).DivisionsX = (paramFreeGrid.GetInt("DivisionsX", 1), 1, 50, 1)
         obj.addProperty(
-            "App::PropertyInteger",
+            "App::PropertyIntegerConstraint",
             QT_TRANSLATE_NOOP("App::Property", "DivisionsY"),
             QT_TRANSLATE_NOOP("App::Property", "Internal divisions"),
             QT_TRANSLATE_NOOP("App::Property", "Number of divisions along the Y axis"),
-        ).DivisionsY = paramFreeGrid.GetInt("DivisionsY", 1)
+        ).DivisionsY = (paramFreeGrid.GetInt("DivisionsY", 1), 1, 50, 1)
         obj.addProperty(
             "App::PropertyPercent",
             QT_TRANSLATE_NOOP("App::Property", "DivisionHeight"),
@@ -144,33 +190,44 @@ class StorageBoxObject(StorageObject):
         ).MagnetOption = self.magnetOptions
         obj.MagnetOption = self.magnetOptions[paramFreeGrid.GetInt("MagnetOption", 0)]
         obj.addProperty(
-            "App::PropertyInteger",
+            "App::PropertyIntegerConstraint",
             QT_TRANSLATE_NOOP("App::Property", "PositionX"),
             QT_TRANSLATE_NOOP("App::Property", "Position on grid"),
             QT_TRANSLATE_NOOP(
                 "App::Property", "Box position on the grid in the X axis.\nStarts at zero."
             ),
-        ).PositionX = 0
+        ).PositionX = (0, 0, 50, 1)
         obj.addProperty(
-            "App::PropertyInteger",
+            "App::PropertyIntegerConstraint",
             QT_TRANSLATE_NOOP("App::Property", "PositionY"),
             QT_TRANSLATE_NOOP("App::Property", "Position on grid"),
             QT_TRANSLATE_NOOP(
                 "App::Property", "Box position on the grid in the Y axis.\nStarts at zero."
             ),
-        ).PositionY = 0
+        ).PositionY = (0, 0, 50, 1)
 
     def onChanged(self, obj, prop):
+        """Verify length properties are inside limits and disable properties if needed"""
+
+        self.check_limits(obj, prop)
+
         if prop == "MagnetOption":
             obj.setEditorMode("MagnetDiameter", obj.MagnetOption == "noMagnets")
             obj.setEditorMode("MagnetHeight", obj.MagnetOption == "noMagnets")
-        elif prop in ["PositionX", "PositionY"]:
-            # FIXME: find a way to avoid shape re-computation when only
-            # the position on the grid is changed, maybe cache the shape
+        elif prop == "PositionX":
+            self.offset_changed = True
             if obj.PositionX < 0:
                 obj.PositionX = 0
+        elif prop == "PositionY":
+            self.offset_changed = True
             if obj.PositionY < 0:
                 obj.PositionY = 0
+        # PositionX is created before PositionY, so check both exist before translating
+        if (
+            prop in ["PositionX", "PositionY"]
+            and hasattr(obj, "PositionX")
+            and hasattr(obj, "PositionY")
+        ):
             obj.AttachmentOffset = Placement(
                 Vector(obj.PositionX * SPACING, obj.PositionY * SPACING, 3.2),
                 Rotation(0.0, 0.0, 0.0),
@@ -180,28 +237,27 @@ class StorageBoxObject(StorageObject):
         """Create a box using the object properties as parameters."""
         box = StorageBox.StorageBox()
         # Size
-        x = max(1, obj.Width)
-        y = max(1, obj.Depth)
-        # No point having less than 2.6[mm] because geometry remains the same
-        z = max(2.6, obj.Height.getValueAs("mm").Value)
+        x = obj.Width
+        y = obj.Depth
+        z = obj.Height.getValueAs("mm").Value
         # dividers = divisions - 1
-        box.divisions_x = max(1, obj.DivisionsX)
-        box.divisions_y = max(1, obj.DivisionsY)
+        box.divisions_x = obj.DivisionsX
+        box.divisions_y = obj.DivisionsY
         try:
-            box.division_height = min(100, max(1, obj.DivisionHeight)) / 100.0
+            box.division_height = obj.DivisionHeight / 100.0  # users can't input bad data
         except AttributeError:
             box.division_height = 1.0
         FreeCAD.Console.PrintWarning(box.division_height)
         # Features
         box.closed_front = not obj.BoxOpenFront
         box.ramp = obj.BoxRamp  # scoop
-        depth = max(1, obj.BoxGripDepth.getValueAs("mm").Value)
-        if obj.BoxGrip and depth > 0:
-            box.grip_depth = depth
+        bg_depth = obj.BoxGripDepth.getValueAs("mm").Value
+        if obj.BoxGrip and bg_depth > 0:
+            box.grip_depth = bg_depth
         box.floor_support = obj.FloorSupport
         # Magnets
-        mag_d = min(max(1, obj.MagnetDiameter.getValueAs("mm").Value), 6.9)
-        mag_h = min(max(0.2, obj.MagnetHeight.getValueAs("mm").Value), 3.4)
+        mag_d = obj.MagnetDiameter.getValueAs("mm").Value
+        mag_h = obj.MagnetHeight.getValueAs("mm").Value
         if obj.MagnetOption == "noMagnets":
             box.magnets = False
             box.magnets_corners_only = True
@@ -229,9 +285,21 @@ class BitCartridgeHolderObject(StorageBoxObject):
 
     def __init__(self, obj):
         """Initialize storage box object, add properties."""
+
         super().__init__(obj)
+
         self.storageType = "BitCartridgeHolder"
-        self.magnetOptions = ["allIntersections", "cornersOnly", "noMagnets"]
+
+        # Define value constraints for length properties
+        self.min_len_constraints = {
+            "Height": 2.6,  # No point having less than 2.6[mm] because geometry remains the same
+            "MagnetDiameter": 1,
+            "MagnetHeight": 0.2,
+            "BoxGripDepth": 1,
+            "SideLength": 10,
+        }
+        self.max_len_constraints = {"MagnetDiameter": 6.9, "MagnetHeight": 3.4}
+
         for prop in [
             "DivisionsX",
             "DivisionsY",
@@ -253,15 +321,15 @@ class BitCartridgeHolderObject(StorageBoxObject):
         """Create a bit cartridge holder using the object properties as parameters."""
         bit_c_h = StorageBox.BitCartridgeHolder()
         # Size
-        size = max(10, obj.SideLength.getValueAs("mm").Value)
-        x = max(1, obj.Width)
-        y = max(1, obj.Depth)
-        z = max(0, obj.Height.getValueAs("mm").Value)
+        size = obj.SideLength.getValueAs("mm").Value
+        x = obj.Width
+        y = obj.Depth
+        z = obj.Height.getValueAs("mm").Value
         # Features
         bit_c_h.floor_support = obj.FloorSupport
         # Magnets
-        mag_d = min(max(1, obj.MagnetDiameter.getValueAs("mm").Value), 6.9)
-        mag_h = min(max(0.2, obj.MagnetHeight.getValueAs("mm").Value), 3.4)
+        mag_d = obj.MagnetDiameter.getValueAs("mm").Value
+        mag_h = obj.MagnetHeight.getValueAs("mm").Value
         if obj.MagnetOption == "noMagnets":
             bit_c_h.magnets = False
             bit_c_h.magnets_corners_only = True
@@ -288,11 +356,18 @@ class StorageGridObject(StorageObject):
 
     def __init__(self, obj):
         """Initialize storage grid object, add properties."""
+
         super().__init__(obj)
+
         self.storageType = "StorageGrid"
+
+        # Define value constraints for length properties
+        self.min_len_constraints = {"MagnetDiameter": 1, "MagnetHeight": 0.2}
+        self.max_len_constraints = {"MagnetDiameter": 6.9, "MagnetHeight": 3.4}
+
         obj.Proxy = self
-        obj.Depth = paramFreeGrid.GetInt("GridDepth", 2)
-        obj.Width = paramFreeGrid.GetInt("GridWidth", 3)
+        obj.Depth = (paramFreeGrid.GetInt("GridDepth", 2), 1, 50, 1)
+        obj.Width = (paramFreeGrid.GetInt("GridWidth", 3), 1, 50, 1)
         obj.addProperty(
             "App::PropertyBool",
             QT_TRANSLATE_NOOP("App::Property", "CornerConnectors"),
@@ -321,6 +396,10 @@ class StorageGridObject(StorageObject):
         ).IncludeMagnets = paramFreeGrid.GetBool("IncludeMagnets", True)
 
     def onChanged(self, obj, prop):
+        """Verify length properties are inside limits and disable properties if needed"""
+
+        self.check_limits(obj, prop)
+
         if prop == "IncludeMagnets":
             obj.setEditorMode("MagnetDiameter", not obj.IncludeMagnets)
             obj.setEditorMode("MagnetHeight", not obj.IncludeMagnets)
@@ -329,15 +408,15 @@ class StorageGridObject(StorageObject):
         """Create a grid using the object properties as parameters."""
         grid = StorageGrid.StorageGrid()
         # Size
-        x = max(1, obj.Width)
-        y = max(1, obj.Depth)
+        x = obj.Width
+        y = obj.Depth
         # Features
         grid.corner_connectors = obj.CornerConnectors
         grid.is_subtractive = obj.IsSubtractive
         extra_bottom = max(0, obj.ExtraBottomMaterial.getValueAs("mm").Value)
         # Magnets
-        mag_d = min(max(1, obj.MagnetDiameter.getValueAs("mm").Value), 6.9)
-        mag_h = min(max(0.2, obj.MagnetHeight.getValueAs("mm").Value), 3.4)
+        mag_d = obj.MagnetDiameter.getValueAs("mm").Value
+        mag_h = obj.MagnetHeight.getValueAs("mm").Value
         grid.magnets = obj.IncludeMagnets
 
         # NOTE: `x` and `y` are swapped in order to maintain
@@ -346,6 +425,8 @@ class StorageGridObject(StorageObject):
 
     def execute(self, obj):
         """Create the requested storage grid object."""
+
+        # Update position when attached-to object changes position.
         obj.positionBySupport()
         obj.Shape = self.generate_grid(obj)
         obj.Label = self.descriptionStr(obj)
