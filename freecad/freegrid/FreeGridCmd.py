3ff8d94e5796e48b5c7220ecb71a757ba7c172d4
hasecilu <hasecilu@tuta.io>
2025-01-21 13:02:21 -0600

    fix(grid): change property mode with correct parameter

--- freecad/freegrid/FreeGridCmd.py
+++ freecad/freegrid/FreeGridCmd.py
@@ -396,7 +396,7 @@ class StorageGridObject(StorageObject):
         if prop == "IncludeMagnets":
             obj.setEditorMode("MagnetDiameter", not obj.IncludeMagnets)
             obj.setEditorMode("MagnetHeight", not obj.IncludeMagnets)
-        elif prop == "IsSubtractive" and hasattr(self, "ExtraBottomMaterial"):
+        elif prop == "IsSubtractive" and hasattr(obj, "ExtraBottomMaterial"):
             obj.setEditorMode("ExtraBottomMaterial", not obj.IsSubtractive)
 
     def generate_grid(self, obj) -> Part.Shape:
